c1578f635433aada0000161d5b8553030ecbd2bb
sinaya naik <sinayanaik@gmail.com>
2021-08-19 22:01:38 +0530

    decimal to binary converter

---
+++ script.js
@@ -0,0 +1,35 @@
+// program to convert decimal to binary
+function convertToBinary(x) {
+    let text = "";
+    let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        text = text+`<p>Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}</p>`
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    text = text + `<hr><h3>Binary: ${bin}</h3>`;
+    return text;
+}
+
+function convert() {
+    let DecimalNumber = document.querySelector("#enter-number").value;
+    const ParentBox = document.querySelector("#parent-box");
+    let process = convertToBinary(DecimalNumber);
+    let OutputText = `<p>steps:</p><br>Decimal number = ${DecimalNumber}<hr>${process}`
+    // check if already ChildBox present
+    if(document.querySelectorAll('.childbox').length !=0){
+        ParentBox.removeChild(document.querySelectorAll('.childbox')[0])
+    }
+    // child box
+    const ChildBox = document.createElement("div");
+    ChildBox.classList.add("box", "childbox");
+    // output para
+    const OutputPara = document.createElement("p");
+    OutputPara.innerHTML = OutputText;
+    // appendchild
+    ChildBox.appendChild(OutputPara);
+    ParentBox.appendChild(ChildBox);
+}
---
+++ test.js
@@ -0,0 +1,15 @@
+function convertToBinary(x) {
+    let text = "";
+    let bin = 0;
+    let rem, i = 1, step = 1;
+    while (x != 0) {
+        rem = x % 2;
+        text = text+`<p>Step ${step++}: ${x}/2, Remainder = ${rem}, Quotient = ${parseInt(x/2)}</p>`
+        x = parseInt(x / 2);
+        bin = bin + rem * i;
+        i = i * 10;
+    }
+    console.log(`Binary: ${bin}`);
+    return text;
+}
+console.log(convertToBinary(50));
